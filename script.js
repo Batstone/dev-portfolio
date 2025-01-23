20d923e217108fdcef749dfbe42e034dcbf8ade4
Adam Batstone <adam.batstone@gmail.com>
2025-01-23 16:05:55 -0500

    header styles

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 "use strict";
-const mobileButton = document.querySelector("#mobile-btn");
+const mobileButtonOpen = document.querySelector("#mobile-btn-open");
 const nav = document.querySelector("#mobile-nav");
-const screenReaderText = mobileButton.querySelector(".sr-only");
+const screenReaderText = mobileButtonOpen.querySelector(".sr-only");
 const firstLink = nav.querySelector("ul li:first-child a");
 const lastLink = nav.querySelector("ul li:last-child a");
-mobileButton.addEventListener("click", (e) => {
+mobileButtonOpen.addEventListener("click", (e) => {
     e.preventDefault();
-    mobileButton.classList.toggle("active");
+    mobileButtonOpen.classList.toggle("active");
     nav.classList.toggle("active");
     const isExpanded = nav.classList.contains("active");
-    mobileButton.setAttribute("aria-expanded", isExpanded.toString());
+    mobileButtonOpen.setAttribute("aria-expanded", isExpanded.toString());
     screenReaderText.textContent = isExpanded ? "Close mobile navigation" : "Expand mobile navigation";
-    console.log("hereee");
-    //   if (isExpanded) {
-    //     firstLink.focus();
-    //   }
+    if (isExpanded) {
+        firstLink.focus();
+    }
 });
+document.addEventListener("keydown", function () { });
--- script.ts
+++ script.ts
@@ -1,21 +1,23 @@
-const mobileButton = document.querySelector<HTMLElement>("#mobile-btn")!;
+const mobileButtonOpen = document.querySelector<HTMLElement>("#mobile-btn-open")!;
 const nav = document.querySelector<HTMLElement>("#mobile-nav")!;
-const screenReaderText = mobileButton.querySelector<HTMLElement>(".sr-only")!;
+const screenReaderText = mobileButtonOpen.querySelector<HTMLElement>(".sr-only")!;
 const firstLink = nav.querySelector<HTMLElement>("ul li:first-child a")!;
 const lastLink = nav.querySelector<HTMLElement>("ul li:last-child a")!;
 
-mobileButton.addEventListener("click", (e) => {
+mobileButtonOpen.addEventListener("click", (e) => {
   e.preventDefault();
-  mobileButton.classList.toggle("active");
+  mobileButtonOpen.classList.toggle("active");
   nav.classList.toggle("active");
 
   const isExpanded = nav.classList.contains("active");
 
-  mobileButton.setAttribute("aria-expanded", isExpanded.toString());
+  mobileButtonOpen.setAttribute("aria-expanded", isExpanded.toString());
 
   screenReaderText.textContent = isExpanded ? "Close mobile navigation" : "Expand mobile navigation";
-  console.log("hereee");
-  //   if (isExpanded) {
-  //     firstLink.focus();
-  //   }
+
+  if (isExpanded) {
+    firstLink.focus();
+  }
 });
+
+document.addEventListener("keydown", function () {});
